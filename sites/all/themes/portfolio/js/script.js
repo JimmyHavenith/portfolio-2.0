a2fdb373a3cb71c0b1b46c40f971a57706af2223
Jimmy Havenith <jimhave@outlook.com>
2016-08-06 15:52:32 +0200

    Css & JS Animation
    Animation for projects

--- sites/all/themes/portfolio/js/script.js
+++ sites/all/themes/portfolio/js/script.js
@@ -3,6 +3,8 @@
 
   $(document).ready(function()
   {
+
+    //Animation filtre
     $('.header-button-filter').click(function()
     {
       if($('.content').hasClass('filter-not-display')){
@@ -14,5 +16,28 @@
       }
       return false;
     });
+
+    //Positionnement titre+tag projet
+
+    if($('.isotope-container li').hasClass('web')){
+      $('.web.isotope-element').addClass('project-web');
+    }
+
+    if($('.isotope-container li').hasClass('print')){
+      $('.print.isotope-element').addClass('project-print');
+    }
+
+    if($('.isotope-element').hasClass('project-web')){
+      var heightBlocInfoWeb = $('.project-web').height() / 2;
+      var heightInfoWeb = $('.project-info').height() / 2;
+      var middleHeightBlocInfoWeb = heightBlocInfoWeb - heightInfoWeb;
+      $('.project-web .project-info').css('padding-top', middleHeightBlocInfoWeb);
+    }
+    if($('.isotope-element').hasClass('project-print')){
+      var heightBlocInfoPrint = $('.project-print').height() / 2;
+      var heightInfoPrint = $('.project-info').height() / 2;
+      var middleHeightBlocInfoPrint = heightBlocInfoPrint - heightInfoPrint;
+      $('.project-print .project-info').css('padding-top', middleHeightBlocInfoPrint);
+    }
   });
 })(jQuery);
