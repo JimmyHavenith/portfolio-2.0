2eb3b73453e5a31eb6f4f78592251956165762dc
Jimmy Havenith <jimhave@outlook.com>
2016-08-12 12:59:38 +0200

    Modification Homepage
    Issues with plugin Isotope when the website is online .. So, I changed
    the interface without the plugin !

--- sites/all/themes/portfolio/js/script.js
+++ sites/all/themes/portfolio/js/script.js
@@ -4,41 +4,12 @@
   $(document).ready(function()
   {
 
-    // Animation filtre
-    $('.header-button-filter').click(function()
-    {
-      if($('.content').hasClass('filter-not-display')){
-        $('.content').removeClass('filter-not-display');
-        $('.content').addClass('filter-display');
-      } else{
-        $('.content').removeClass('filter-display');
-        $('.content').addClass('filter-not-display');
-      }
-      return false;
-    });
+    // Vertical-align bloc info Homepage
+    var heightInfoBloc = $('.project-info').height() / 2;
+    var heightInfo = $('.project-info-div').height() / 2;
+    var middleHeight = heightInfoBloc - heightInfo;
+    $('.project-info-div').css('padding-top', middleHeight);
 
-    // Positionnement titre+tag projet
-
-    if($('.isotope-container li').hasClass('web')){
-      $('.web.isotope-element').addClass('project-web');
-    }
-
-    if($('.isotope-container li').hasClass('print')){
-      $('.print.isotope-element').addClass('project-print');
-    }
-
-    if($('.isotope-element').hasClass('project-web')){
-      var heightBlocInfoWeb = $('.project-web').height() / 2;
-      var heightInfoWeb = $('.project-info').height() / 2;
-      var middleHeightBlocInfoWeb = heightBlocInfoWeb - heightInfoWeb;
-      $('.project-web .project-info').css('padding-top', middleHeightBlocInfoWeb);
-    }
-    if($('.isotope-element').hasClass('project-print')){
-      var heightBlocInfoPrint = $('.project-print').height() / 2;
-      var heightInfoPrint = $('.project-info').height() / 2;
-      var middleHeightBlocInfoPrint = heightBlocInfoPrint - heightInfoPrint;
-      $('.project-print .project-info').css('padding-top', middleHeightBlocInfoPrint);
-    }
 
     // Menu HAMBURGER
     $('.header-button-menu').click(function(e){
@@ -54,15 +25,6 @@
         $('.body-black').css('display', "block");
       }
     });
-
-    $('.views-field-field-project-highlight img').each(function(){
-  		$(this).removeAttr('width')
-  		$(this).removeAttr('height');
-	  });
-
-    // Min-height body
-    // var bodyHeight = $(window).height() / 2;
-    // $('.content').css('min-height', bodyHeight);
 
   });
 })(jQuery);
---
+++ sites/all/themes/portfolio/templates/node/node--404.tpl.php
@@ -0,0 +1,15 @@
+<p>
+  coucou
+</p>
+
+<h2>Contact</h2>
+<p itemprop="contactPoints">
+  <span itemprop="address" itemscope itemtype="http://schema.org/PostalAddress">
+    <span itemprop="streetAddress">10, place du Temple Neuf</span><br />
+    <span itemprop="postalCode">67000</span>
+    <span itemprop="addressLocality">Strasbourg</span>,
+    <meta itemprop="addressCountry" content="FR" />France
+  </span><br />
+  <span itemprop="telephone">0954965050</span>
+</p>
+<p><a href="http://alsacreations.fr/" itemprop="url">Site Web</a></p>
